0a02baecfbf496676f8ed67c03fd6e36a3fd10fa
Marta <marti.arslanova@gmail.com>
2022-03-22 00:40:30 +0400

    feature: add desktop markup

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.querySelector('.portfolio__wrapper').addEventListener('wheel', function (event) {
+document.querySelector('.portfolio__area-scroll').addEventListener('wheel', function (event) {
   if (event.deltaY != 0) {
     this.scrollLeft += event.deltaY;
     event.preventDefault();
